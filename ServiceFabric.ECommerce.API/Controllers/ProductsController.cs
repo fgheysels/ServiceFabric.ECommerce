a19635216ad4126b137e4436ec4ad73430e640a8
Frederik Gheysels <frederik.gheysels@telenet.be>
2018-01-04 22:16:14 +0100

    Added web api project

---
+++ ServiceFabric.ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceFabric.ECommerce.API.Model;
+
+namespace ServiceFabric.ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new[] { new Product() { Id = Guid.NewGuid(), Name = "Fake Product", Description = "Fake Product" } };
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Product product)
+        {
+        }
+
+    }
+}
---
+++ ServiceFabric.ECommerce.API/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ServiceFabric.ECommerce.API.Model
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+        [JsonProperty("isAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
