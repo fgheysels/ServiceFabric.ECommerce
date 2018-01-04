8c2f6f1e52d2674c2805f0819fc0843373d8aad1
Frederik Gheysels <frederik.gheysels@telenet.be>
2018-01-04 20:54:43 +0100

    Added ProductCatalog.Model project

---
+++ ServiceFabric.ECommerce.ProductCatalog.Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFabric.ECommerce.ProductCatalog.Model
+{
+    public class Product
+    {
+    }
+}
