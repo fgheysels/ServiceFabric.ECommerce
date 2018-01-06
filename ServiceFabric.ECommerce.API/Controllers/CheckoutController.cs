71b809a99e7497398ece95a1fd730db0f1066737
Frederik Gheysels <frederik.gheysels@telenet.be>
2018-01-06 22:29:13 +0100

    Extended web api with CheckoutController & model.

---
+++ ServiceFabric.ECommerce.API/Controllers/CheckoutController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using ServiceFabric.ECommerce.CheckoutService.Model;
+using CheckoutSummary = ServiceFabric.ECommerce.API.Model.CheckoutSummary;
+
+namespace ServiceFabric.ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CheckoutController : Controller
+    {
+        [Route("{userId}")]
+        public async Task<CheckoutSummary> Checkout(string userId)
+        {
+            var summary = await GetCheckoutService().Checkout(userId);
+
+            return new CheckoutSummary(summary);
+        }
+
+        [Route("history/{userId")]
+        public async Task<IEnumerable<CheckoutSummary>> GetHistory(string userId)
+        {
+            var history = await GetCheckoutService().GetOrderHistory(userId);
+
+            return history.Select(h => new CheckoutSummary(h));
+        }
+
+        private static ICheckoutService GetCheckoutService()
+        {
+            return ServiceProxy.Create<ICheckoutService>(new Uri("fabric:/ServiceFabric.EComerce/ServiceFabric.ECommerce.CheckoutService"),
+                                                         new ServicePartitionKey(0));
+        }
+
+    }
+}
---
+++ ServiceFabric.ECommerce.API/Model/CheckoutProduct.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ServiceFabric.ECommerce.API.Model
+{
+    public class CheckoutProduct
+    {
+        [JsonProperty("productId")]
+        public Guid ProductId { get; set; }
+
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckoutProduct"/> class.
+        /// </summary>
+        public CheckoutProduct(CheckoutService.Model.CheckoutProduct product )
+        {
+            this.ProductId = product.Product.Id;
+            this.ProductName = product.Product.Name;
+            this.Quantity = product.Quantity;
+            this.Price = product.Price;
+        }
+    }
+
+    public class CheckoutSummary
+    {
+        [JsonProperty("products")]
+        public List<CheckoutProduct> Products { get; set; }
+
+        [JsonProperty("totalprice")]
+        public decimal TotalPrice { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckoutSummary"/> class.
+        /// </summary>
+        public CheckoutSummary ( CheckoutService.Model.CheckoutSummary summary)
+        {
+            this.TotalPrice = summary.TotalPrice;
+            this.Date = summary.Date;
+            Products = new List<CheckoutProduct>(summary.Products.Select(p => new CheckoutProduct(p)));
+        }
+
+    }
+}
