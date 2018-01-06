6961ff82cb4beeeee3101d32ad6bc1934efa9e16
Frederik Gheysels <frederik.gheysels@telenet.be>
2018-01-06 22:29:46 +0100

    implemented checkout method in CheckoutService

--- ServiceFabric.ECommerce.CheckoutService/CheckoutService.cs
+++ ServiceFabric.ECommerce.CheckoutService/CheckoutService.cs
@@ -4,20 +4,27 @@ using System.Fabric;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
 using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
+using ServiceFabric.ECommerce.CheckoutService.Model;
+using ServiceFabric.ECommerce.ProductCatalog.Model;
+using UserActor.Interfaces;
 
 namespace ServiceFabric.ECommerce.CheckoutService
 {
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class CheckoutService : StatefulService
+    internal sealed class CheckoutService : StatefulService, ICheckoutService
     {
         public CheckoutService(StatefulServiceContext context)
-            : base(context)
-        { }
+            : base(context) { }
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -28,41 +35,70 @@ namespace ServiceFabric.ECommerce.CheckoutService
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
-            return new ServiceReplicaListener[0];
+            return this.CreateServiceRemotingReplicaListeners();
         }
 
-        /// <summary>
-        /// This is the main entry point for your service replica.
-        /// This method executes when this replica of your service becomes primary and has write status.
-        /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
+        public async Task<CheckoutSummary> Checkout(string userId)
         {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
+            var result = new CheckoutSummary();
+            result.Date = DateTime.Now;
+            result.Products = new List<CheckoutProduct>();
 
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+            // Call the UserActor for the current user and get his shopping cart.
+            var userActor = GetUserActor(userId);
 
-            while (true)
+            var cartContent = await userActor.GetCart();
+
+            var catalogService = GetProductCatalogService();
+
+            // TODO:this is a chatty interface; create a chunky method.
+            foreach (var item in cartContent)
             {
-                cancellationToken.ThrowIfCancellationRequested();
+                var product = await catalogService.GetProduct(item.Key);
 
-                using (var tx = this.StateManager.CreateTransaction())
+                var checkoutProduct = new CheckoutProduct()
                 {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+                    Product = product,
+                    Price = product.Price,
+                    Quantity = item.Value
+                };
 
-                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
-                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
-
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
-
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                result.Products.Add(checkoutProduct);
             }
+
+            // Add the checkout to the history.
+            await AddCheckoutToHistory(result);
+
+            await userActor.ClearCart();
+
+            return result;
+        }
+
+        private async Task AddCheckoutToHistory(CheckoutSummary summary)
+        {
+            // TODO: shouldn't we have the history of a user in the actor ?
+            var history = await StateManager.GetOrAddAsync<IReliableDictionary<DateTime, CheckoutSummary>>("history");
+
+            using (var tx = StateManager.CreateTransaction())
+            {
+                await history.AddAsync(tx, summary.Date, summary);
+                await tx.CommitAsync();
+            }
+        }
+
+        private static IUserActor GetUserActor(string userId)
+        {
+            return ActorProxy.Create<IUserActor>(new ActorId(userId), new Uri("fabric:/ServiceFabric.ECommerce/ServiceFabric.ECommerce.UserActorService"));
+        }
+
+        private static IProductCatalogService GetProductCatalogService()
+        {
+            return ServiceProxy.Create<IProductCatalogService>(new Uri("fabric:/ServiceFabric.ECommerce/ServiceFabric.ECommerce.ProductCatalogService"), new ServicePartitionKey(0));
+        }
+
+        public async Task<IEnumerable<CheckoutSummary>> GetOrderHistory(string userId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
